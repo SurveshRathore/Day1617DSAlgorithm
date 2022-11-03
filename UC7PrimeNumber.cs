5d6c63d78863ee029ead988c92abb5d110180a89
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-03 14:06:52 +0530

    UC7 prime numbers ranges from 0 to 1000

---
+++ UC7PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1617Algorithm
+{
+    internal class UC7PrimeNumber
+    {
+        public void generatePrimeNum()
+        {
+            Console.WriteLine("The prime numbers from 0 to 1000 is: \n2");
+            bool prime = false;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    else
+                    {
+                        prime = true;
+                    }
+                }
+                if(prime)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
