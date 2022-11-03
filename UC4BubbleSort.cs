7cc88822120b6fc2a8b1540e9c5b40f8911f60b9
SurveshRathore <113424413+SurveshRathore@users.noreply.github.com>
2022-11-03 14:09:22 +0530

    UC4 Reads the integer and sort them using bubble sort

---
+++ UC4BubbleSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1617Algorithm
+{
+    internal class UC4BubbleSort
+    {
+        public void bubbleSort()
+        {
+            //read the element from the user
+            Console.Write("Enter the number of elements: ");
+            int element = Convert.ToInt32(Console.ReadLine());
+            int[] ints = new int[element];
+            Console.Write("Enter the {0} integer elements.", element);
+            for(int i = 0; i < element; i++)
+            {
+                ints[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //display the list
+            Console.Write("The integer elements are: ");
+            for (int i = 0; i < element; i++)
+            {
+                Console.Write(ints[i] + " ");
+            }
+
+            //sort the list
+            for(int i = 0; i < element - 1; i++)
+            {
+                for(int j = 0; j < element - i - 1; j++)
+                {
+                    if (ints[j] > ints[j+1])
+                    {
+                        int temp = ints[j];
+                        ints[j] = ints[j+1];
+                        ints[j+1] = temp;
+                    }
+                }
+            }
+
+            //display the list
+            Console.Write("The integer elements in the ascending order are: ");
+            for (int i = 0; i < element; i++)
+            {
+                Console.Write(ints[i] + " ");
+            }
+
+        }
+        
+    }
+}
